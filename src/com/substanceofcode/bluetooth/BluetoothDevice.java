708695096109f94be9560a219feb07b016b47714
gdjones <gdjones@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2008-02-05 22:02:18 +0000

    Attempting to recommit...

--- src/com/substanceofcode/bluetooth/BluetoothDevice.java
+++ src/com/substanceofcode/bluetooth/BluetoothDevice.java
@@ -1,57 +1,14 @@
-/*
- * BluetoothDevice.java
- *
- * Copyright (C) 2005-2006 Tommi Laukkanen
- * http://www.substanceofcode.com
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
 package com.substanceofcode.bluetooth;
 
+import java.io.IOException;
+
 /**
+ * Defines the methods we expect on a bluetooth capable device
+ * @author gareth
  *
- * @author Tommi Laukkanen
  */
-public class BluetoothDevice {
-    
-    protected String address;
-    protected String alias;
-   
-    /** 
-     * Explicit no arg constructor to allow for mock implementations
-     * of subclasses
-     *
-     */
-    public BluetoothDevice(){
-    	
-    }
-    
-    /** Creates a new instance of BluetoothDevice */
-    public BluetoothDevice(String address, String alias) {
-        this.alias = alias;
-        this.address = address;
-    }
-    
-    public String getAddress() {
-        String url;
-        url =  address;
-        return url;
-    }
-    
-    public String getAlias() {
-        return alias;
-    }
+public interface BluetoothDevice extends Device{
+
+    public void connect() throws IOException ;
+    public void disconnect() throws IOException ;
 }
