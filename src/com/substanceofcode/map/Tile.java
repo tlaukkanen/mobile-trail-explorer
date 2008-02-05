5f82efe6eefd3442e9ef3f8a8146dcae57df0a2a
gdjones <gdjones@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2008-02-05 20:54:04 +0000

    Added byte array to allow tile data to be easily written to the filesystem.

--- src/com/substanceofcode/map/Tile.java
+++ src/com/substanceofcode/map/Tile.java
@@ -2,6 +2,8 @@ package com.substanceofcode.map;
 
 import javax.microedition.lcdui.Image;
 
+
+
 /**
  * Stores the Meta-data relating to a single map tile, and it's retrieval status
  * 
@@ -17,11 +19,17 @@ public class Tile {
 
     public String url;
 
+    // Ideally we don't want to have to store this along with the Image object, 
+    // as it is cleaner to store an Image created from this byteArray. 
+    // However, we also want to write the tile to the filesystem
+    // Which will require either writing a PngImageWriter, or, outputting this byte [] ;-).
+    // The downside is that this could make Tiles too big, so perhaps we should delete
+    // this once the tile has been written correctly to the filesystem?
+    private byte [] imageByteArray=null; 
   
+    private String destDir;
 
-    public String destDir;
-
-    public String destFile;
+    private String destFile;
 
     public String cacheKey;
     
@@ -39,10 +47,54 @@ public class Tile {
         this.cacheKey = storename + "-" + z + "-" + x + "-" + y;
     }
     
+    public Tile(String name){
+        this.x = 0;
+        this.y = 1;
+        this.z = 2;
+        this.url = "";
+      
+        this.destDir = "";
+        this.destFile = "";
+        this.cacheKey = "" + "-" + z + "-" + x + "-" + y;
+    }
+    
+    public void setImageByteArray(byte[] in){
+        
+          //  byte[] x=in;
+            
+          //  this.imageByteArray= new byte[in.length];
+            this.imageByteArray=in;
+        
+        }
+    
+    public byte [] getImageByteArray(){
+        return this.imageByteArray;
+    }
+    
+    public void clearImageByteArray(){
+        this.imageByteArray=null;
+    }
+    
     public void setImage(Image image){
         this.image=image;
     }
     public Image getImage(){
         return image;
+    }
+
+    public String getDestDir() {
+        return destDir;
+    }
+
+    public void setDestDir(String destDir) {
+        this.destDir = destDir;
+    }
+
+    public String getDestFile() {
+        return destFile;
+    }
+
+    public void setDestFile(String destFile) {
+        this.destFile = destFile;
     }
 }
