6d17f2a12c7d5bfeca1ab4cfa8a201acb9792a5b
gdjones <gdjones@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2008-02-13 07:24:18 +0000

    Attempt to get data from internal gps devices.

--- src/com/substanceofcode/gpsdevice/Jsr179Device.java
+++ src/com/substanceofcode/gpsdevice/Jsr179Device.java
@@ -1,12 +1,15 @@
 package com.substanceofcode.gpsdevice;
 
+import java.util.Date;
 import java.util.Vector;
 
+import javax.microedition.location.AddressInfo;
 import javax.microedition.location.Criteria;
 import javax.microedition.location.Location;
 import javax.microedition.location.LocationException;
 import javax.microedition.location.LocationListener;
 import javax.microedition.location.LocationProvider;
+import javax.microedition.location.QualifiedCoordinates;
 
 import com.substanceofcode.gps.GpsGPGSA;
 import com.substanceofcode.gps.GpsPosition;
@@ -23,17 +26,19 @@ import com.substanceofcode.util.StringUtil;
 public class Jsr179Device extends GpsDeviceImpl implements Runnable,
         LocationListener {
 
-
+    private static final String JSR179MIMETYPE="application/X-jsr179-location-nmea";
     private final String logPrefix = "Jsr179: ";
     private Thread thread;
     private LocationProvider locationProvider;
     // GPS position variables
+    private  boolean usingExternalGPS=false; 
     private String extraInfo = "";
-
+    private GpsGPGSA gpgsa=null;
+    GpsPosition gp=null;
     protected GpsPositionParser parser;
 
     public Jsr179Device() {
-
+        
     }
 
     public Jsr179Device(String address, String alias) {
@@ -147,22 +152,61 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable,
         return "JSR179 Location API";
     }
 
-    public GpsGPGSA getGPGSA() {
-        return parser.getGPGSA();
-    }
+   
 
     public String[] getParserMetrics() {
         return parser.getMetrics();
     }
 
     public GpsPosition getPosition() {
+       // Logger.debug("getPosition called");
+        if(usingExternalGPS){
+            return getParserPosition();
+        }else{
+            return getJsr179Position();
+        }
+    }
+    public GpsGPGSA getGPGSA() {
+        //Logger.debug("getGPGSA called");
+        if(usingExternalGPS){
+            return getParserGPGSA();
+        }else{
+            return getJsr179GPGSA();
+        }
+    }
+    
+    public GpsGPGSA getParserGPGSA(){
+      //  Logger.debug("getParserGPGSA called");
+        return parser.getGPGSA();
+    }
+
+    
+    public GpsPosition getParserPosition() {
+    //    Logger.debug("getParserPosition called");
         return parser.getGpsPosition();
     }
-
-    public int getSatelliteCount() {
-        return parser.getSatelliteCount();
+    public GpsPosition getJsr179Position() {
+        //System.out.println("getJsr179Position called");
+        //System.out.println(gp.altitude);
+        //System.out.println(gp.latitude);
+        //System.out.println(gp.longitude);
+        //System.out.println(gp.speed);
+        //System.out.println(gp.course);
+       // System.out.println(gp.date);
+        
+        
+        return gp;
     }
-
+    public GpsGPGSA getJsr179GPGSA() {
+       // System.out.println("getJsr179GPGSA called");
+      //  System.out.println(gpgsa.getFixtype());
+      //  System.out.println(gpgsa.getHdop());
+     //   System.out.println(gpgsa.getPdop());
+    //    System.out.println(gpgsa.getVdop());
+        
+        return gpgsa;
+    }
+  
     public Vector getSatellites() {
         // these methods do not work until a sentence has been parsed.
         return parser.getSatellites();
@@ -173,7 +217,44 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable,
      * 
      */
     public void locationUpdated(LocationProvider provider, Location location) {
-        extraInfo = location.getExtraInfo("application/X-jsr179-location-nmea");
+        
+        extraInfo = location.getExtraInfo(JSR179MIMETYPE);
+        
+        //Guessing that if there is no nmea data present, the Location API is giving us an internal
+        //GPS or a network
+        if(extraInfo!=null){
+           usingExternalGPS=true;
+           Logger.debug("usingExternalGps is "+usingExternalGPS);
+        }
+        
+        float course=location.getCourse();
+        float speed=location.getSpeed();
+        
+        QualifiedCoordinates qc=location.getQualifiedCoordinates();
+        float altitude=qc.getAltitude();
+        float hdop=qc.getHorizontalAccuracy();
+        double lat=  qc.getLatitude();
+        double lon=  qc.getLongitude();
+        float vdop= qc.getVerticalAccuracy();
+        
+        
+        System.out.println(altitude);
+        System.out.println(hdop);
+        System.out.println(lat);
+        System.out.println(lon);
+        System.out.println(vdop);
+        
+        //These might be useful later...
+       // boolean isValid=location.isValid();               
+       // AddressInfo addressInfo=location.getAddressInfo();
+        //int locationMethod=location.getLocationMethod();
+        
+        long timestamp=location.getTimestamp();
+        
+        gp=new GpsPosition("",(short)course,lon,lat,speed,(double)altitude,new Date(timestamp));
+        gpgsa=new GpsGPGSA(0.0f,hdop,vdop,0);
+        
+         
     }
     /**
      * Called by the Location framework when the status of the provider has changed
@@ -185,10 +266,10 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable,
                 state="Available";
                 break;
             case 2:
-                state="Available";
+                state="Temporarily Unavailable";
                 break;
             case 3:
-                state="Available";
+                state="UnAvailable";
                 break;
         }
         
