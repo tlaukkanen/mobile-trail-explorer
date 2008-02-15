52a97080e686b3e59cf3e8d42122b8002596310c
gdjones <gdjones@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2008-02-15 13:29:17 +0000

    Check API is present before attempting to use it.

--- src/com/substanceofcode/gpsdevice/GpsDeviceFactory.java
+++ src/com/substanceofcode/gpsdevice/GpsDeviceFactory.java
@@ -14,28 +14,31 @@ import com.substanceofcode.tracker.view.Logger;
 public class GpsDeviceFactory {
     /**
      * Create one of the Device implementations
+     * 
      * @param address
      * @param alias
-     * @return The selected Device, or null if other options prevent the chosen device from
-     * being created. Eg jsr179 devices need explicit permission to run
+     * @return The selected Device, or null if other options prevent the chosen
+     *         device from being created. Eg jsr179 devices need explicit
+     *         permission to run
      */
     public static Device createDevice(String address, String alias) {
         Logger.debug("address is " + address);
-        Device dev=null;
-        if ("internal".equals(address) ) {
-            //Jsr179Device requires permission, which it might not get.
-            //In that event we need to abort this creation process so a new
-            //device can be selected.
+        Device dev = null;
+        if ("internal".equals(address)) {
+            // Jsr179Device requires permission, which it might not get.
+            // In that event we need to abort this creation process so a new
+            // device can be selected.
             // Create an internal (non bluetooth) gps device
-            if( Controller.getController().getUseJsr179()){
-                dev = new Jsr179Device(address,alias);            
-                
+            if (GpsUtilities.checkJsr179IsPresent()
+                    && Controller.getController().getUseJsr179()) {
+                dev = new Jsr179Device(address, alias);
+
             }
         } else if ("Mock".equals(address)) {
-           dev =new MockGpsDevice(address, alias);
+            dev = new MockGpsDevice(address, alias);
         } else {
 
-            dev =new BluetoothGPSDeviceImpl(address, alias);
+            dev = new BluetoothGPSDeviceImpl(address, alias);
         }
         return dev;
     }
