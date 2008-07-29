e2cf3c777b3ea149e56d7ff960ff0ee816edf51e
tlaukkanen <tlaukkanen@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2008-07-29 14:39:23 +0000

    Improving configurable shortcut key configuration.

--- src/com/substanceofcode/tracker/view/KeySettingsList.java
+++ src/com/substanceofcode/tracker/view/KeySettingsList.java
@@ -26,32 +26,34 @@ public class KeySettingsList extends List implements CommandListener {
     private static final short CROSSHATCH = 1;
 
     public KeySettingsList() {
-        super("Keys", List.EXCLUSIVE);
+        super("Keys", List.IMPLICIT);
         backCommand = new Command(LocaleManager.getMessage("menu_back"), Command.BACK, 3);
         addCommand(backCommand);
         changeCommand = new Command( LocaleManager.getMessage("menu_change"), Command.SCREEN, 1 );
         addCommand(changeCommand);
         setSelectCommand(changeCommand);
         setCommandListener(this);
-        append( LocaleManager.getMessage("keys_asterisk"), null);
-        append( LocaleManager.getMessage("keys_cross"), null);
+        append( LocaleManager.getMessage("keys_star"), null);
+        append( LocaleManager.getMessage("keys_pound"), null);
     }
 
     public void commandAction(Command cmd, Displayable disp) {
+        Controller controller = Controller.getController();
         if(cmd==backCommand) {
-            Controller.getController().showSettings();
+            controller.showSettings();
         }
         if(cmd==changeCommand) {
             int selected = getSelectedIndex();
             ShortcutsList shortcuts = new ShortcutsList();
             int shortcut = 0;
             if(selected==ASTERISK) {
-                shortcut = Controller.getController().getSettings().getStarShortcut();
+                shortcut = controller.getSettings().getStarShortcut();
             } else if(selected==CROSSHATCH) {
-                shortcut = Controller.getController().getSettings().getPoundShortcut();
+                shortcut = controller.getSettings().getPoundShortcut();
             }
-            shortcuts.setKey((short)selected);
-            shortcuts.setSelectedIndex(shortcut, true);
+            shortcuts.setKey( (short)selected );
+            shortcuts.setSelectedIndex( shortcut, true );
+            controller.showDisplayable( shortcuts );
         }
     }
 
