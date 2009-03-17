603c0e6f961103fd883c2bef2be8f98acd160565
tlaukkanen <tlaukkanen@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2009-03-17 14:51:08 +0000

    Copyright year updated to 2009

--- src/com/substanceofcode/tracker/model/GpxConverter.java
+++ src/com/substanceofcode/tracker/model/GpxConverter.java
@@ -1,7 +1,7 @@
 /*
  * GpxConverter.java
  *
- * Copyright (C) 2005-2008 Tommi Laukkanen
+ * Copyright (C) 2005-2009 Tommi Laukkanen
  * http://www.substanceofcode.com
  *
  * This program is free software; you can redistribute it and/or modify
