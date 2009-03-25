c1f112781976eba5697ffd04ad10adac73d4c404
gdjones <gdjones@2782c49f-9a1a-0410-8ec9-d5952a4cb7f3>
2009-03-25 20:10:07 +0000

    Big change to JSR179Device, taking on board comments from several users. Main change is removing the extra thread.

--- src/com/substanceofcode/gpsdevice/Jsr179Device.java
+++ src/com/substanceofcode/gpsdevice/Jsr179Device.java
@@ -34,79 +34,81 @@ import javax.microedition.location.QualifiedCoordinates;
 import javax.microedition.location.Coordinates;
 
 import com.substanceofcode.bluetooth.Device;
+import com.substanceofcode.gps.GpsGPGSA;
 import com.substanceofcode.gps.GpsPosition;
-import com.substanceofcode.gps.GpsPositionParser;
 import com.substanceofcode.tracker.view.Logger;
 import com.substanceofcode.util.StringUtil;
 
 /**
  * Class to provide jsr179 functionality
- * 
+ *
  * @author gareth
  * @author steinerp
- * 
+ *
  */
-public class Jsr179Device extends GpsDeviceImpl implements Runnable {
-    
+public class Jsr179Device extends GpsDeviceImpl {
+
     private static final String JSR179MIMETYPE = "application/X-jsr179-location-nmea";
     private final String logPrefix = "Jsr179: ";
-    private Thread thread;
 
     // GPS position variables
-    private  boolean usingExternalGPS = false;
+
     private String extraInfo = "";
-    GpsPosition gp = null;
+    //Is this necessary given there is one in GpsDeviceImpl.parser?
+   // GpsPosition gp = null;
     private static Jsr179Device _jsr179Device = null;
-    //protected GpsPositionParser parser;
 
     private static QualifiedCoordinates qc;
-    
+
     private LocationProvider locationProvider;
-    
+
     private final LocationListener locationListener = new LocationListener() {
         public void locationUpdated(LocationProvider provider, Location location) {
-           
-            if (location.isValid()) {
+
+            if (location!=null && location.isValid()) {
                 extraInfo = location.getExtraInfo(JSR179MIMETYPE);
-                
-                //Guessing that if there is no nmea data present, the Location API is giving us an internal
-                //GPS or a network
-                if(extraInfo != null){
-                    usingExternalGPS = true;
-                    //Logger.debug("dbg(): extrainfo:(" + extraInfo + ")");
-                }
-                //   usingExternalGPS=true;
-                //   Logger.debug("using ExternalGps is "+usingExternalGPS);
-                //}
-                // mccormackaj removed the code above. Internal GPS recievers
-                // also give extraInfo so it makes no sense. 
-                // Had to do this to get it working on Blackberry 8820 emulator.
-                
+
                 float course = location.getCourse();
                 float speed = location.getSpeed();
 
                 // convert from m/s to km/h
                 float speedkmh = speed * 3.6f;
-                
+
                 qc = location.getQualifiedCoordinates();
                 float altitude = qc.getAltitude();
                 float hdop = qc.getHorizontalAccuracy();
-                
+
                 double lat =  qc.getLatitude();
                 double lon =  qc.getLongitude();
                 float vdop = qc.getVerticalAccuracy();
-                        
-                //These might be useful later...
-               // boolean isValid=location.isValid();               
-               // AddressInfo addressInfo=location.getAddressInfo();
-                //int locationMethod=location.getLocationMethod();
-                
+
+             //   Logger.debug("JSR179: Qualified Coordinates are:\nalt="+altitude+
+               //         "\nlat="+lat+
+                 //       "\nlon="+lon+
+                   //     "\nvdop="+vdop+
+                     //   "\nhdop="+hdop);
                 long timestamp = location.getTimestamp();
-                gp = new GpsPosition(extraInfo,(short)course,lon,lat,(double)speedkmh,(double)altitude,new Date(timestamp));
-                //gpgsa=new GpsGPGSA(0.0f,hdop,vdop,0);
+                //populate the parser object with the data from the extraInfo string
+
+                //Nokia Phones give the whole nmea string in the extraInfo
+                //So you can use the GpsPosition from the parser for those devices
+                //but... SE devices only return GPGSV sentences in extraInfo
+                //So you need to add that in to a GpsPosition constructed
+                //from the other methods in the Location API
+                //How to do this correctly for both?
+
+                //This works for Nokia Phones (N95 at least)
+                //This reads all available NMEA data into the parser object
+                parseExtraInfo();
+                //For Nokia devices we are done, all the info has been read by the parser
+                //For SE devices we have only got the Satellite Info
+                //We can fix that by adding in a GpsPosition constructed from the API values
+                //Downside for Nokia devices is we are overwriting an existing valid GpsPosition
+                parser.setGpsPosition(new GpsPosition(extraInfo,(short)course,lon,lat,(double)speedkmh,(double)altitude,new Date(timestamp),new GpsGPGSA(0.0f,hdop,vdop,0)));
+
             }
         }
-        
+
         public void providerStateChanged(final javax.microedition.location.LocationProvider provider, final int newState) {
             String state = "";
             switch (newState) {
@@ -118,24 +120,27 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable {
                     break;
                 case javax.microedition.location.LocationProvider.TEMPORARILY_UNAVAILABLE:
                     state = "Temporarily Unavailable";
-                    break;   
+                    break;
             }
             Logger.debug(logPrefix + "State Changed: [" + state + "]");
         }
     };
 
     public Jsr179Device() {
-        
+
     }
 
     protected Jsr179Device(String address, String alias) {
         super(address, alias);
-        init();
-        parser = GpsPositionParser.getPositionParser();
-        thread = new Thread(this);
-        thread.start();
+        //Calling init in the constructor activates the device
+        //We might just want to list the device and use another
+        //init();
     }
-    
+
+    /**
+     * Initialise the jsr179 device, after this method is called locationUpdated
+     * events will be read from the provider and used to construct the device position
+     */
     public void init() {
 
         try {
@@ -168,32 +173,27 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable {
         }
     }
 
-    public void run() {
-        Logger.debug(logPrefix + "Jsr179Device.run() started");
+    /**
+     * Destroy the jsr179Device object. Required if the user changes from
+     * jsr179 to an external gps via settings
+     */
+    public void disconnect(){
+        _jsr179Device=null;
+    }
+    private void parseExtraInfo(){
         Vector nmeaStrings = new Vector();
-        Logger.info(logPrefix + "Starting Jsr179Device.run()");   
+         if (extraInfo != null) {
+            // Logger.debug("parseExtraInfo ["+extraInfo+"]");
+                    StringBuffer output = new StringBuffer();
 
-        while (Thread.currentThread() == thread) {
-
-            try {// Delete this once it's working
-                Thread.sleep(100);
-            } catch (InterruptedException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-
-            try {
-                if (extraInfo != null) {
-                    StringBuffer output = new StringBuffer();                   
-                    synchronized (extraInfo) {
                         if (nmeaStrings.size() == 0 && extraInfo != null
                                 && extraInfo.length() > 0) {
-                            
+
                             nmeaStrings = StringUtil.splitToNMEAVector(extraInfo);
                         }
-                        // Only continue if we have some strings to parse
-                        if (nmeaStrings.size() > 0) {
-
+                        // Only continue if we have some strings to parseNMEA
+                        while (nmeaStrings.size() > 0) {
+                            output.delete(0, output.capacity());
                             //Logger.debug("VectorSize=" + nmeaStrings.size());
                             // pop the first element off the stack (there is no
                             // 'remove' in j2me)
@@ -201,42 +201,29 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable {
                             nmeaStrings.removeElementAt(0);
 
                             try {
-                                // Trim start and end of any NON-Displayable
+                                // Trim start and end of any undesirable
                                 // characters.
-                                while (output.charAt(0) < '!'
-                                        || output.charAt(0) > '~') {
-                                    output.deleteCharAt(0);
-                                }
-                                while (output.charAt(output.length() - 1) < '!'
-                                        || output.charAt(output.length() - 1) > '~') {
-                                    output.deleteCharAt(output.length() - 1);
-                                }
+                                deleteInvalidChars(output, 0);
+                                deleteInvalidChars(output, output.length() - 1);
                             } catch (IndexOutOfBoundsException e) {
-                                // Ignore but don't bother trying to parse, just
+                                // Ignore but don't bother trying to parseNMEA, just
                                 // loop
                                 // around to the next iteration;
                                 continue;
                             }
-                            // only parse items beginning with '$', such as
+                            // only parseNMEA items beginning with '$', such as
                             // "$GPRMC,..."
                             // and "$GPGSA,..." etc...
                             String nmeaString = output.toString();
                             if (parser.isValidNMEASentence(nmeaString)) {
                             //    Logger.debug("String is ok, parsing");
-                                parser.parse(nmeaString);
+                                parser.parseNMEA(nmeaString);
 
                             } else {
-                            //    Logger.debug("String was NOT ok:" + nmeaString);                                
+                                Logger.error("JSR179:String was NOT ok:" + nmeaString);
                             }
-                        }
                     }
-                } else {
                 }
-            } catch (Exception e) {
-                Logger.error(logPrefix + "Exception: " + e.getMessage());
-                e.printStackTrace();
-            }
-        }
     }
 
     public String getAddress() {
@@ -252,21 +239,17 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable {
     }
 
     public GpsPosition getPosition() {
-       // Logger.debug("getPosition called");
-        if(usingExternalGPS){
-            return getParserPosition();
-        } else {
             return getJsr179Position();
-        }
     }
-    
+
     public GpsPosition getParserPosition() {
     //    Logger.debug("getParserPosition called");
         return parser.getGpsPosition();
     }
 
     public GpsPosition getJsr179Position() {
-        return gp;
+        //return gp;
+        return parser.getGpsPosition();
     }
 
     public Vector getSatellites() {
@@ -296,8 +279,22 @@ public class Jsr179Device extends GpsDeviceImpl implements Runnable {
         return _jsr179Device;
     }
 
+    //This relies on qc, which is not initialised until locationUpdated is called
     public static double getCourse(double lat, double lon) {
         Coordinates a = new Coordinates(lat, lon, Float.NaN);
-        return qc.azimuthTo(a);
+        if (qc!=null)
+            return qc.azimuthTo(a);
+        else
+            return 0.0;
+    }
+
+    /*
+     * Delete any characters that are not expected to be in an NMEA sentence.
+     */
+    private void deleteInvalidChars(StringBuffer output, int idx){
+         while (output.charAt(idx) < '!'
+                || output.charAt(idx) > '~') {
+            output.deleteCharAt(idx);
+        }
     }
 }
